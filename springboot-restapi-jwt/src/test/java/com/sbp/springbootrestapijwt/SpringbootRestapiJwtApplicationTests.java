1b632de5726bcc1c27009c4c62a2da079e8d7202
spb.arun.01@gmail.com <arun.dhwaj@smartbitpixel.co.in>
2020-01-22 19:21:37 +0530

    POC for RestAPI and JWT in SpringBoot and MySQL

---
+++ springboot-restapi-jwt/src/test/java/com/sbp/springbootrestapijwt/SpringbootRestapiJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbp.springbootrestapijwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestapiJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
